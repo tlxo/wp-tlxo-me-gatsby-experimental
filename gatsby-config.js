7f8cf76f0fea6915b8169794b7e38c117748a1f7
Toni Laakso <toni@laaxo.com>
2020-09-06 19:23:18 +0300

    Change WPGRAPHQL_URL to wp.tlxo.me/graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       options: {
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://dev-gatsby-source-wordpress-v4.pantheonsite.io/graphql`,
+          `https://wp.tlxo.me/graphql`,
         verbose: true,
         develop: {
           hardCacheMediaFiles: true,
